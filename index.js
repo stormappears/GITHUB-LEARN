e82dd5d6839a5d250505d081d2e0a1eaeff33032
Stormappears <nalayakgram@gmail.com>
2023-12-27 13:30:13 +0530

    add new function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("Hello World")
 console.log("Hello I Added Function for sending email")
 console.log("Hello I Added Function For Accepting Email")
+console.log("This Is Broken Function But I Dont Know Its Broken")
